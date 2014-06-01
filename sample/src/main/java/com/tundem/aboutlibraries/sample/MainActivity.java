77718a0d827ea6da4398d69c6af73789f8c8e252
Mike Penz <mikepenz@gmail.com>
2014-06-01 12:17:27 +0200

    * add sample project

---
+++ sample/src/main/java/com/tundem/aboutlibraries/sample/MainActivity.java
@@ -0,0 +1,131 @@
+package com.tundem.aboutlibraries.sample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.fima.cardsui.objects.CardStack;
+import com.fima.cardsui.views.CardUI;
+import com.tundem.aboutlibraries.Libs;
+import com.tundem.aboutlibraries.entity.Library;
+import com.tundem.aboutlibraries.sample.cardsui.LibraryCard;
+
+import java.util.List;
+
+
+public class MainActivity extends Activity {
+
+    private CardUI mCardView;
+    private Libs libs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        libs = Libs.getInstance(this, R.string.class.getFields());
+
+        // init CardView
+        mCardView = (CardUI) findViewById(R.id.cardsview);
+        mCardView.setSwipeable(false);
+
+        setDefined();
+    }
+
+    private void setAll() {
+        mCardView.clearCards();
+
+        mCardView.addStack(new CardStack(" "));
+
+        List<Library> libraries = libs.getLibraries();
+        for (Library lib : libraries) {
+            mCardView.addCard(new LibraryCard(this, lib));
+        }
+        mCardView.addStack(new CardStack(" "));
+
+        mCardView.refresh();
+    }
+
+    private void setExternal() {
+        mCardView.clearCards();
+
+        mCardView.addStack(new CardStack(" "));
+
+        List<Library> libraries = libs.getExternLibraries();
+        for (Library lib : libraries) {
+            mCardView.addCard(new LibraryCard(this, lib));
+        }
+        mCardView.addStack(new CardStack(" "));
+
+        mCardView.refresh();
+    }
+
+    private void setInternal() {
+        mCardView.clearCards();
+
+        mCardView.addStack(new CardStack(" "));
+
+        List<Library> libraries = libs.getInternLibraries();
+        for (Library lib : libraries) {
+            mCardView.addCard(new LibraryCard(this, lib));
+        }
+        mCardView.addStack(new CardStack(" "));
+
+        mCardView.refresh();
+    }
+
+    private void setDefined() {
+        mCardView.clearCards();
+
+        mCardView.addStack(new CardStack(" "));
+        mCardView.addCard(new LibraryCard(this, "AndroidIconify"));
+        mCardView.addCard(new LibraryCard(this, "ActiveAndroid"));
+        mCardView.addCard(new LibraryCard(this, "CardsUI"));
+        mCardView.addCard(new LibraryCard(this, "FButton"));
+        mCardView.addCard(new LibraryCard(this, "Crouton"));
+        mCardView.addCard(new LibraryCard(this, "HoloGraphLibrary"));
+        mCardView.addCard(new LibraryCard(this, "ShowcaseView"));
+        mCardView.addCard(new LibraryCard(this, "NineOldAndroids"));
+        mCardView.addCard(new LibraryCard(this, "AndroidViewpagerIndicator"));
+        mCardView.addStack(new CardStack(" "));
+
+        mCardView.refresh();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_all) {
+            setAll();
+            return true;
+        } else if (id == R.id.action_external) {
+            setExternal();
+            return true;
+        } else if (id == R.id.action_internal) {
+            setInternal();
+            return true;
+        } else if (id == R.id.action_defined) {
+            setDefined();
+            return true;
+        } else if (id == R.id.action_opensource) {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/mikepenz/AboutLibraries"));
+            startActivity(browserIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ sample/src/main/java/com/tundem/aboutlibraries/sample/cardsui/LibraryCard.java
@@ -0,0 +1,77 @@
+package com.tundem.aboutlibraries.sample.cardsui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import com.fima.cardsui.objects.Card;
+import com.tundem.aboutlibraries.Libs;
+import com.tundem.aboutlibraries.entity.Library;
+import com.tundem.aboutlibraries.sample.R;
+
+public class LibraryCard extends Card {
+    private Context ctx;
+
+    private Library library;
+
+    public LibraryCard(Context context, String libraryName) {
+        super(libraryName);
+
+        library = Libs.getInstance(context).getLibrary(libraryName);
+    }
+
+    public LibraryCard(Context context, Library library) {
+        super(library.getLibraryName());
+
+        this.library = library;
+    }
+
+    @Override
+    public View getCardContent(final Context context) {
+        this.ctx = context;
+
+        View view = LayoutInflater.from(context).inflate(R.layout.card_library, null);
+
+        ((TextView) view.findViewById(R.id.libraryname)).setText(library.getLibraryName());
+        ((TextView) view.findViewById(R.id.librarycreator)).setText(library.getAuthor());
+        ((TextView) view.findViewById(R.id.libraryversion)).setText(library.getLibraryVersion());
+        ((TextView) view.findViewById(R.id.description)).setText(library.getLibraryDescription());
+        ((TextView) view.findViewById(R.id.description)).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (!library.getLibraryWebsite().equals("")) {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(library.getLibraryWebsite()));
+                    context.startActivity(browserIntent);
+                }
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public boolean convert(View view) {
+
+        ((TextView) view.findViewById(R.id.libraryname)).setText(library.getLibraryName());
+        ((TextView) view.findViewById(R.id.librarycreator)).setText(library.getAuthor());
+        ((TextView) view.findViewById(R.id.libraryversion)).setText(library.getLibraryVersion());
+        ((TextView) view.findViewById(R.id.description)).setText(library.getLibraryDescription());
+        ((TextView) view.findViewById(R.id.description)).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (!library.getLibraryWebsite().equals("")) {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(library.getLibraryWebsite()));
+                    ctx.startActivity(browserIntent);
+                }
+            }
+        });
+
+        return false;
+    }
+}
