5b86f5faea1f49259d9bc909b32b1daef939a872
Mike Penz <mikepenz@gmail.com>
2014-09-08 21:18:35 +0200

    * Add Detect class to be able to detect libs by their classpath

---
+++ library/src/main/java/com/tundem/aboutlibraries/detector/Detect.java
@@ -0,0 +1,42 @@
+package com.tundem.aboutlibraries.detector;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.text.TextUtils;
+
+import com.tundem.aboutlibraries.entity.Library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mikepenz on 08.09.14.
+ */
+public class Detect {
+    public static List<Library> detect(Context mCtx, List<Library> libraries) {
+        ArrayList<Library> foundLibraries = new ArrayList<Library>();
+        // Loop through known libraries
+        for (Library library : libraries) {
+            if (!TextUtils.isEmpty(library.getClassPath())) {
+                try {
+                    Context ctx = mCtx.createPackageContext(mCtx.getPackageName(),
+                            Context.CONTEXT_INCLUDE_CODE | Context.CONTEXT_IGNORE_SECURITY);
+                    Class<?> clazz = Class.forName(library.getClassPath(), false, ctx.getClassLoader());
+
+                    // Detected a library!!!
+                    if (clazz != null) {
+                        foundLibraries.add(library);
+                    }
+                } catch (ClassNotFoundException e) {
+                    //e.printStackTrace();
+                } catch (PackageManager.NameNotFoundException e) {
+                    //e.printStackTrace();
+                }
+            }
+        }
+        // Only return AppSource if app has a library
+        //return libraries.size() > 0 ? new AppSource(pkg, libraries) : null;
+
+        return foundLibraries;
+    }
+}
