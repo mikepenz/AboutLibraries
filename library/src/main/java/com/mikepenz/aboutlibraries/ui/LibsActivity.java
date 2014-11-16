30e0264d67f8a8eeaf5387be65f7d34311182f6d
Mike Penz <mikepenz@gmail.com>
2014-11-16 16:46:50 +0100

    * Fix formatting * Also allow to set StatusBar Color if a Custom Theme was set

--- library/src/main/java/com/mikepenz/aboutlibraries/ui/LibsActivity.java
+++ library/src/main/java/com/mikepenz/aboutlibraries/ui/LibsActivity.java
@@ -18,62 +18,64 @@ import com.mikepenz.aboutlibraries.util.Colors;
  */
 public class LibsActivity extends ActionBarActivity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		//set the theme
-		boolean customTheme = false;
-		Bundle bundle = getIntent().getExtras();
-		if (bundle != null) {
-			int themeId = bundle.getInt(Libs.BUNDLE_THEME, -1);
-			if (themeId != -1) {
-				customTheme = true;
-				setTheme(themeId);
-			}
-		}
-		if (!customTheme) {
-			setTheme(R.style.Theme_AppCompat);
-		}
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_opensource);
-		String title = "";
-		if (bundle != null) {
-			title = bundle.getString(Libs.BUNDLE_TITLE);
-		}
-		LibsFragment fragment = new LibsFragment();
-		fragment.setArguments(bundle);
-		ActionBar ab = getSupportActionBar();
-		if (ab != null) {
-			if (!customTheme) {
-				Colors colors = bundle == null ? null : (Colors) bundle.getParcelable(Libs.BUNDLE_COLORS);
-				if (colors != null) {
-					ab.setBackgroundDrawable(new ColorDrawable(colors.appBarColor));
-					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-						getWindow().setStatusBarColor(colors.statusBarColor);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        //set the theme
+        boolean customTheme = false;
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            int themeId = bundle.getInt(Libs.BUNDLE_THEME, -1);
+            if (themeId != -1) {
+                customTheme = true;
+                setTheme(themeId);
+            }
+        }
+        if (!customTheme) {
+            setTheme(R.style.Theme_AppCompat);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opensource);
+        String title = "";
+        if (bundle != null) {
+            title = bundle.getString(Libs.BUNDLE_TITLE);
+        }
+        LibsFragment fragment = new LibsFragment();
+        fragment.setArguments(bundle);
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            // Set StatusBar Color by Code
+            Colors colors = bundle == null ? null : (Colors) bundle.getParcelable(Libs.BUNDLE_COLORS);
+            if (colors != null) {
+                ab.setBackgroundDrawable(new ColorDrawable(colors.appBarColor));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    getWindow().setStatusBarColor(colors.statusBarColor);
+                }
 
-				} else ab.setBackgroundDrawable(null);
-			}
-			ab.setDisplayHomeAsUpEnabled(true);
-			if (TextUtils.isEmpty(title)) {
-				ab.setDisplayShowTitleEnabled(false);
-			} else {
-				ab.setDisplayShowTitleEnabled(true);
-				ab.setTitle(title);
-			}
-			ab.setDisplayUseLogoEnabled(true);
-		}
-		FragmentManager fragmentManager = getSupportFragmentManager();
-		fragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit();
-	}
+            } else ab.setBackgroundDrawable(null);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case android.R.id.home: {
-				LibsActivity.this.finish();
-				return true;
-			}
-			default:
-				return false;
-		}
-	}
+            // SetUp ActionBar
+            ab.setDisplayHomeAsUpEnabled(true);
+            if (TextUtils.isEmpty(title)) {
+                ab.setDisplayShowTitleEnabled(false);
+            } else {
+                ab.setDisplayShowTitleEnabled(true);
+                ab.setTitle(title);
+            }
+            ab.setDisplayUseLogoEnabled(true);
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                LibsActivity.this.finish();
+                return true;
+            }
+            default:
+                return false;
+        }
+    }
 }
