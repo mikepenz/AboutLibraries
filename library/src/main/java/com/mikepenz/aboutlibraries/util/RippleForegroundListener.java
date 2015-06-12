7231bfc4cd0ee5ade4b6d1989ed360d0124a9705
wKovacs64 <justin.r.hall@gmail.com>
2015-06-12 12:56:21 -0600

    Generify RippleForegroundListener

--- library/src/main/java/com/mikepenz/aboutlibraries/util/RippleForegroundListener.java
+++ library/src/main/java/com/mikepenz/aboutlibraries/util/RippleForegroundListener.java
@@ -1,40 +1,42 @@
 package com.mikepenz.aboutlibraries.util;
 
-import android.support.v7.widget.CardView;
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * Created by mikepenz on 16.04.15.
  */
-public class RippleForegroundListener implements View.OnTouchListener {
-    CardView cardView;
+public class RippleForegroundListener<T extends View> implements View.OnTouchListener {
+    T view;
 
-    public RippleForegroundListener setCardView(CardView cardView) {
-        this.cardView = cardView;
+    public RippleForegroundListener setLayout(T view) {
+        this.view = view;
         return this;
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        // Convert to card view coordinates. Assumes the host view is
-        // a direct child and the card view is not scrollable.
+        // Convert to view coordinates. Assumes the host view is
+        // a direct child and the view is not scrollable.
         float x = event.getX() + v.getLeft();
         float y = event.getY() + v.getTop();
 
-        if (android.os.Build.VERSION.SDK_INT >= 21) {
-            // Simulate motion on the card view.
-            cardView.drawableHotspotChanged(x, y);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // Simulate motion on the view.
+            view.drawableHotspotChanged(x, y);
         }
 
-        // Simulate pressed state on the card view.
+        // Simulate pressed state on the view.
         switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
-                cardView.setPressed(true);
+                view.setPressed(true);
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
-                cardView.setPressed(false);
+                view.setPressed(false);
                 break;
         }
 
