4f7ca23f3aeb8fc70e8dbfdebb9121681fd2c4fe
Mike Penz <mikepenz@gmail.com>
2015-08-04 09:59:38 +0200

    * improve GenericsUtil to locate the `R` class

--- library/src/main/java/com/mikepenz/aboutlibraries/util/GenericsUtil.java
+++ library/src/main/java/com/mikepenz/aboutlibraries/util/GenericsUtil.java
@@ -1,6 +1,7 @@
 package com.mikepenz.aboutlibraries.util;
 
 import android.content.Context;
+import android.text.TextUtils;
 
 import com.mikepenz.aboutlibraries.Libs;
 
@@ -10,14 +11,9 @@ import com.mikepenz.aboutlibraries.Libs;
 public class GenericsUtil {
 
     public static String[] getFields(Context ctx) {
-        Class rClass = resolveRClass(ctx.getPackageName());
-
-        if (rClass != null) {
-            for (Class c : rClass.getClasses()) {
-                if (c.getName().endsWith("string")) {
-                    return Libs.toStringArray(c.getFields());
-                }
-            }
+        Class rStringClass = resolveRClass(ctx.getPackageName());
+        if (rStringClass != null) {
+            return Libs.toStringArray(rStringClass.getFields());
         }
         return new String[0];
     }
@@ -28,22 +24,15 @@ public class GenericsUtil {
      * @param packageName
      * @return
      */
-    private static Class resolveRClass(String packageName) {
-        try {
-            return Class.forName(packageName + ".R");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            return Class.forName(packageName.replace(".debug", "") + ".R");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            return Class.forName(packageName.replace(".release", "") + ".R");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+    protected static Class resolveRClass(String packageName) {
+        do {
+            try {
+                return Class.forName(packageName + ".R$string");
+            } catch (ClassNotFoundException e) {
+                packageName = packageName.contains(".") ? packageName.substring(0, packageName.lastIndexOf('.')) : "";
+            }
+        } while(!TextUtils.isEmpty(packageName));
+
         return null;
     }
 }
