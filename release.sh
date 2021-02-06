e3c456ce8e20ff9bfa6dfd86d00a90ece9758dac
Mike Penz <mikepenz@gmail.com>
2021-02-06 10:40:43 +0100

    - rename modules to their respective artifactId coordinate

--- release.sh
+++ release.sh
@@ -4,9 +4,9 @@
 
 if [ "$1" = "release" ];
 then
-    ./gradlew library-core:publishReleasePublicationToSonatypeRepository -Plibrary_core_only
-    ./gradlew library:publishReleasePublicationToSonatypeRepository -x test -x lint -Plibrary_only
-    ./gradlew library-definitions:publishReleasePublicationToSonatypeRepository -x test -x lint -Plibrary_definitions_only
+    ./gradlew aboutlibraries-core:publishReleasePublicationToSonatypeRepository -Plibrary_core_only
+    ./gradlew aboutlibraries:publishReleasePublicationToSonatypeRepository -x test -x lint -Plibrary_only
+    ./gradlew aboutlibraries-definitions:publishReleasePublicationToSonatypeRepository -x test -x lint -Plibrary_definitions_only
 else
     //TODO
 fi
