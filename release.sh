febbdae76ad85d1e8cdc4f5af22efad7a187887c
Mike Penz <mikepenz@gmail.com>
2021-02-06 10:34:07 +0100

    - adjust build setup to directly publish to sonatype - remove bintray - adjust release script setup

--- release.sh
+++ release.sh
@@ -4,9 +4,9 @@
 
 if [ "$1" = "release" ];
 then
-    ./gradlew library-core:bintrayUpload -Plibrary_core_only
-    ./gradlew library:bintrayUpload -x test -x lint -Plibrary_only
-    ./gradlew library-definitions:bintrayUpload -x test -x lint -Plibrary_definitions_only
+    ./gradlew library-core:publishReleasePublicationToSonatypeRepository -Plibrary_core_only
+    ./gradlew library:publishReleasePublicationToSonatypeRepository -x test -x lint -Plibrary_only
+    ./gradlew library-definitions:publishReleasePublicationToSonatypeRepository -x test -x lint -Plibrary_definitions_only
 else
     //TODO
 fi
