0d30163acef2b2066e4c70a12d5e25fee3f33718
Mike Penz <mikepenz@gmail.com>
2021-11-06 21:22:28 +0100

    - move library list item to common sourceSet

---
+++ aboutlibraries-compose/src/commonMain/kotlin/com/mikepenz/aboutlibraries/ui/compose/SharedLibraries.kt
@@ -0,0 +1,82 @@
+package com.mikepenz.aboutlibraries.ui.compose
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Badge
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.mikepenz.aboutlibraries.entity.Library
+import com.mikepenz.aboutlibraries.ui.compose.util.author
+
+@Composable
+internal fun Library(
+    library: Library,
+    showAuthor: Boolean = true,
+    showVersion: Boolean = true,
+    showLicenseBadges: Boolean = true,
+    onClick: () -> Unit
+) {
+    val typography = MaterialTheme.typography
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                onClick.invoke()
+            }
+            .padding(
+                start = 16.dp,
+                top = 16.dp,
+                end = 16.dp,
+                bottom = 16.dp
+            )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = library.name,
+                style = typography.h6,
+                modifier = Modifier
+                    .padding(top = 4.dp)
+                    .weight(1f),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            val version = library.artifactVersion
+            if (version != null && showVersion) {
+                Text(
+                    version,
+                    modifier = Modifier.padding(start = 8.dp),
+                    style = typography.body2,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+        val author = library.author
+        if (showAuthor && author.isNotBlank()) {
+            Text(
+                text = author,
+                style = typography.body2,
+            )
+        }
+        if (showLicenseBadges && library.licenses.isNotEmpty()) {
+            Row(modifier = Modifier.padding(top = 8.dp)) {
+                library.licenses.forEach {
+                    Badge(
+                        modifier = Modifier.padding(end = 4.dp),
+                        backgroundColor = MaterialTheme.colors.primary
+                    ) {
+                        Text(text = it.name)
+                    }
+                }
+            }
+        }
+    }
+}
