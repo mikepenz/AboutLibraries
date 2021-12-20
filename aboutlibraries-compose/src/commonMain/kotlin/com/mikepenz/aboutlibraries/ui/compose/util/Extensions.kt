d66b1520b6a3569ab7fa146d1c78f861ddd2718c
Mike Penz <mikepenz@gmail.com>
2021-12-20 12:04:37 +0100

    - introduce `AndroidView` to theme license ad html in dialog - add dialog themeing to better match Material3 style -

--- aboutlibraries-compose/src/commonMain/kotlin/com/mikepenz/aboutlibraries/ui/compose/util/Extensions.kt
+++ aboutlibraries-compose/src/commonMain/kotlin/com/mikepenz/aboutlibraries/ui/compose/util/Extensions.kt
@@ -1,6 +1,13 @@
 package com.mikepenz.aboutlibraries.ui.compose.util
 
 import com.mikepenz.aboutlibraries.entity.Library
+import com.mikepenz.aboutlibraries.entity.License
 
 val Library.author: String
     get() = developers.takeIf { it.isNotEmpty() }?.map { it.name }?.joinToString(", ") ?: organization?.name ?: ""
+
+internal val License.htmlReadyLicenseContent: String?
+    get() = licenseContent?.replace("\n", "<br />")
+
+internal val License.strippedLicenseContent: String?
+    get() = licenseContent?.replace("<br />", "\n")?.replace("<br/>", "\n")
