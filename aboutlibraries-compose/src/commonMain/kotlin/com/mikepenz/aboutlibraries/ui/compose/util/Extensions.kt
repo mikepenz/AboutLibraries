7c18108adb5b7ae45014003dc490b847b22ba361
Mike Penz <mikepenz@gmail.com>
2021-12-20 12:16:21 +0100

    - make helper extension properties accessible

--- aboutlibraries-compose/src/commonMain/kotlin/com/mikepenz/aboutlibraries/ui/compose/util/Extensions.kt
+++ aboutlibraries-compose/src/commonMain/kotlin/com/mikepenz/aboutlibraries/ui/compose/util/Extensions.kt
@@ -6,8 +6,8 @@ import com.mikepenz.aboutlibraries.entity.License
 val Library.author: String
     get() = developers.takeIf { it.isNotEmpty() }?.map { it.name }?.joinToString(", ") ?: organization?.name ?: ""
 
-internal val License.htmlReadyLicenseContent: String?
+val License.htmlReadyLicenseContent: String?
     get() = licenseContent?.replace("\n", "<br />")
 
-internal val License.strippedLicenseContent: String?
+val License.strippedLicenseContent: String?
     get() = licenseContent?.replace("<br />", "\n")?.replace("<br/>", "\n")
