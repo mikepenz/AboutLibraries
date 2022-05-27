e29bb89b16336becd597c4b9fd229d69bce04cd6
Mike Penz <mikepenz@gmail.com>
2022-05-27 13:31:26 +0200

    - introduce ability to adjust data in `Libs`   - Adjust `Libs` to be data class -> offering the `copy` API   - Adjust `Libs` to offer constructor to pass in custom list of libraries and licenses

--- aboutlibraries-core/src/commonMain/kotlin/com/mikepenz/aboutlibraries/Libs.kt
+++ aboutlibraries-core/src/commonMain/kotlin/com/mikepenz/aboutlibraries/Libs.kt
@@ -8,44 +8,10 @@ import com.mikepenz.aboutlibraries.util.parseData
  * The [Libs] class is the main access point to the generated data of the plugin.
  * Provides accessors for the [Library] and [License] lists, containing all the dependency information for the module.
  */
-class Libs internal constructor(
-    stringData: String? = null
+data class Libs internal constructor(
+    val libraries: List<Library>,
+    val licenses: Set<License>,
 ) {
-
-    private val _libraries = mutableListOf<Library>()
-    private val _licenses = mutableSetOf<License>()
-
-    /**
-     *
-     */
-    val libraries: List<Library>
-        get() = _libraries
-
-
-    val licenses: Set<License>
-        get() = _licenses
-
-    /**
-     * init method
-     */
-    init {
-        val (libraries, licenses) = if (stringData != null) {
-            parseData(stringData)
-        } else {
-            throw IllegalStateException(
-                """
-                    Please provide the required library data via the available APIs.
-                    Depending on the platform this can be done for example via `LibsBuilder().withJson()`.
-                    For Android there exists an `LibsBuilder.withContext()`, automatically loading the `aboutlibraries.json` file from the `raw` resources folder.
-                    When using compose or other parent modules, please check their corresponding APIs.
-                """.trimIndent()
-            )
-        }
-
-        _libraries.addAll(libraries.sortedBy { it.name.lowercase() })
-        _licenses.addAll(licenses)
-    }
-
     /**
      * Builder used to automatically parse and interpret the generated library data from the plugin.
      */
@@ -64,7 +30,20 @@ class Libs internal constructor(
          * Build the [Libs] instance with the applied configuration.
          */
         fun build(): Libs {
-            return Libs(_stringData)
+            val data = _stringData
+            val (libraries, licenses) = if (data != null) {
+                parseData(data)
+            } else {
+                throw IllegalStateException(
+                    """
+                    Please provide the required library data via the available APIs.
+                    Depending on the platform this can be done for example via `LibsBuilder().withJson()`.
+                    For Android there exists an `LibsBuilder.withContext()`, automatically loading the `aboutlibraries.json` file from the `raw` resources folder.
+                    When using compose or other parent modules, please check their corresponding APIs.
+                """.trimIndent()
+                )
+            }
+            return Libs(libraries.sortedBy { it.name.lowercase() }, licenses.toMutableSet())
         }
     }
 }
