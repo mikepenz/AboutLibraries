47748cb08bfedfd7eb880183f912ae3d45b7e49a
Mike Penz <opensource@mikepenz.dev>
2023-07-08 12:58:01 +0200

    - first draft of a new dependency collection logic based on the gradle dependency report

--- plugin-build/plugin/src/main/kotlin/com/mikepenz/aboutlibraries/plugin/AboutLibrariesCollectorTask.kt
+++ plugin-build/plugin/src/main/kotlin/com/mikepenz/aboutlibraries/plugin/AboutLibrariesCollectorTask.kt
@@ -1,13 +1,16 @@
 package com.mikepenz.aboutlibraries.plugin
 
-import com.mikepenz.aboutlibraries.plugin.model.CollectedContainer
-import com.mikepenz.aboutlibraries.plugin.util.DependencyCollector
-import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.*
+import com.mikepenz.aboutlibraries.plugin.util.DependencyReportCollector
+import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.diagnostics.DependencyReportTask
+import org.gradle.api.tasks.diagnostics.internal.DependencyReportRenderer
 import java.io.File
 
 @CacheableTask
-abstract class AboutLibrariesCollectorTask : DefaultTask() {
+abstract class AboutLibrariesCollectorTask : DependencyReportTask() {
 
     @Internal
     protected val extension = project.extensions.getByName("aboutLibraries") as AboutLibrariesExtension
@@ -18,28 +21,15 @@ abstract class AboutLibrariesCollectorTask : DefaultTask() {
     @Input
     val filterVariants = extension.filterVariants
 
-    /** holds the collected set of dependencies*/
-    @Internal
-    protected lateinit var collectedDependencies: CollectedContainer
-
     @OutputFile
     val dependencyCache: File = File(File(project.buildDir, "generated/aboutLibraries/").also {
         it.mkdirs()
     }, "dependency_cache.json")
 
-    /**
-     * Collect the dependencies via the available configurations for the current project
-     */
-    fun configure() {
-        project.evaluationDependsOnChildren()
-        collectedDependencies = DependencyCollector(includePlatform, filterVariants).collect(project)
-    }
-
-    @TaskAction
-    fun action() {
-        if (!::collectedDependencies.isInitialized) {
-            configure()
-        }
-        dependencyCache.writeText(groovy.json.JsonOutput.toJson(collectedDependencies))
+    fun setRenderer(renderer: DependencyReportCollector) {
+        renderer.dependencyCache = dependencyCache
+        renderer.includePlatform = includePlatform
+        renderer.filterVariants = filterVariants
+        setRenderer(renderer as DependencyReportRenderer)
     }
 }
