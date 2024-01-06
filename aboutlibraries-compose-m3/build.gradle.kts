83ce8caecc00bb2610ee6240317adde085acca28
Mike Penz <opensource@mikepenz.dev>
2024-01-06 10:27:13 +0100

    - further adjust stability

--- aboutlibraries-compose-m3/build.gradle.kts
+++ aboutlibraries-compose-m3/build.gradle.kts
@@ -100,7 +100,6 @@ kotlin {
                 implementation(compose.ui)
                 implementation(compose.foundation)
                 implementation(compose.material3)
-                implementation(libs.kotlinx.collections)
             }
         }
         val commonTest by getting
