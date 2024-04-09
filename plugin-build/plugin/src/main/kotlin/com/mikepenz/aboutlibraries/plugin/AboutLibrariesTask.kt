137eeb8d99b5b7c2fb3758ba206e090155ae24c2
Mike Penz <opensource@mikepenz.dev>
2024-04-09 15:37:28 +0200

    - add new API to allow some licenses only for specific libraries

--- plugin-build/plugin/src/main/kotlin/com/mikepenz/aboutlibraries/plugin/AboutLibrariesTask.kt
+++ plugin-build/plugin/src/main/kotlin/com/mikepenz/aboutlibraries/plugin/AboutLibrariesTask.kt
@@ -1,12 +1,17 @@
 package com.mikepenz.aboutlibraries.plugin
 
+import com.mikepenz.aboutlibraries.plugin.mapping.Library
 import com.mikepenz.aboutlibraries.plugin.mapping.License
 import com.mikepenz.aboutlibraries.plugin.model.writeToDisk
 import com.mikepenz.aboutlibraries.plugin.util.forLicense
-import org.gradle.api.tasks.*
+import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
 import org.slf4j.LoggerFactory
 import java.io.File
-import java.util.*
+import java.util.Locale
 
 @CacheableTask
 abstract class AboutLibrariesTask : BaseAboutLibrariesTask() {
@@ -44,14 +49,34 @@ abstract class AboutLibrariesTask : BaseAboutLibrariesTask() {
                 val name = it.name.lowercase(Locale.ENGLISH)
                 val url = it.url?.lowercase(Locale.ENGLISH)
                 if (!(allowedLicenses.contains(id)
-                            || allowedLicenses.contains(name)
-                            || (url?.isNotEmpty() == true && allowedLicenses.contains(url)))
+                        || allowedLicenses.contains(name)
+                        || (url?.isNotEmpty() == true && allowedLicenses.contains(url)))
                 ) {
                     missing.add(it)
                 }
             }
 
-            if (missing.isEmpty()) {
+            val missingMapped = mutableMapOf<License, List<Library>>()
+            if (allowedLicensesMap.isNotEmpty()) {
+                missing.forEach {
+                    val id = it.spdxId?.lowercase(Locale.ENGLISH) ?: it.hash.lowercase(Locale.ENGLISH)
+
+                    val libsForLicense = allowedLicensesMap[id] ?: allowedLicensesMap[name]
+                    if (libsForLicense != null) {
+                        val notAllowed = result.libraries.forLicense(it).filter { lib ->
+                            !(libsForLicense.contains(lib.uniqueId) || libsForLicense.contains(lib.groupId) || libsForLicense.contains(lib.artifactId))
+                        }
+                        if (notAllowed.isNotEmpty()) {
+                            missingMapped[it] = notAllowed
+                        }
+                    }
+                }
+            } else {
+                missing.forEach { missingMapped[it] = result.libraries.forLicense(it) }
+            }
+
+
+            if (missingMapped.isEmpty()) {
                 LOGGER.info("No libraries detected using a license not allowed.")
             } else {
                 val message = StringBuilder()
@@ -59,11 +84,9 @@ abstract class AboutLibrariesTask : BaseAboutLibrariesTask() {
                     message.appendLine("=======================================")
                 }
                 message.appendLine("Detected usage of not allowed licenses!")
-                missing.forEach {
-                    message.appendLine("-> License: ${it.name} | ${it.spdxId ?: "-"} (${it.url}), used by:")
-                    result.libraries.forLicense(it).forEach { lib ->
-                        message.appendLine("    ${lib.uniqueId}")
-                    }
+                missingMapped.forEach { (license, libraries) ->
+                    message.appendLine("-> License: ${license.name} | ${license.spdxId ?: "-"} (${license.url}), used by:")
+                    libraries.forEach { lib -> message.appendLine("    ${lib.uniqueId}") }
                 }
                 repeat(2) {
                     message.appendLine("=======================================")
