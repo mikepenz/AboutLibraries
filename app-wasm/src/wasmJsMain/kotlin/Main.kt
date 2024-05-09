cebd1d54f453aae73076c7fc2328c1e0ad09c9ae
Mike Penz <opensource@mikepenz.dev>
2024-05-09 16:46:38 +0200

    - add handling of clicks to the wasm demo - update wasm demo and remove now obsolete files - add CI build to publish wasm

--- app-wasm/src/wasmJsMain/kotlin/Main.kt
+++ app-wasm/src/wasmJsMain/kotlin/Main.kt
@@ -2,13 +2,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.window.CanvasBasedWindow
 import com.mikepenz.aboutlibraries.Libs
+import com.mikepenz.aboutlibraries.entity.Library
 import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
-import com.mikepenz.`app-wasm`.generated.resources.Res
+import com.mikepenz.aboutlibraries.ui.compose.LicenseDialog
+import com.mikepenz.aboutlibraries.ui.compose.util.htmlReadyLicenseContent
+import com.mikepenz.app_wasm.generated.resources.Res
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jetbrains.compose.resources.ExperimentalResourceApi
@@ -28,10 +34,38 @@ fun main() {
                     }
                 }
 
+                val uriHandler = LocalUriHandler.current
+                val openDialog = remember { mutableStateOf<Library?>(null) }
+
                 LibrariesContainer(
                     libraries.value,
-                    Modifier.fillMaxSize()
+                    Modifier.fillMaxSize(),
+                    onLibraryClick = { library ->
+                        val license = library.licenses.firstOrNull()
+                        if (!license?.url.isNullOrBlank()) {
+                            license?.url?.also {
+                                try {
+                                    uriHandler.openUri(it)
+                                } catch (t: Throwable) {
+                                    println("Failed to open url: ${it}")
+                                }
+                            }
+                        } else if (!license?.htmlReadyLicenseContent.isNullOrBlank()) {
+                            openDialog.value = library
+                        }
+                    },
                 )
+
+
+                val library = openDialog.value
+                if (library != null) {
+                    // TODO use https://android-review.googlesource.com/c/platform/frameworks/support/+/3024604
+                    LicenseDialog(library, body = {
+                        Text(library.licenses.firstOrNull()?.htmlReadyLicenseContent ?: "")
+                    }) {
+                        openDialog.value = null
+                    }
+                }
             }
         }
     }
