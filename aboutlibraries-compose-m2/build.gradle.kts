16649988a38c3e7217c42c14b1b6975b4cae79b1
Mike Penz <opensource@mikepenz.dev>
2024-05-28 08:06:07 +0200

    - java 17 some more

--- aboutlibraries-compose-m2/build.gradle.kts
+++ aboutlibraries-compose-m2/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {
-            jvmTarget = "11"
+            jvmTarget = "17"
 
             if (project.findProperty("composeCompilerReports") == "true") {
                 freeCompilerArgs += listOf(
