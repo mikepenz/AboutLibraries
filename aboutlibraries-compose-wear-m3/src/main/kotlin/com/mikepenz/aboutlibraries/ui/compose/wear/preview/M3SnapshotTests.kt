12907cd8b0ee35bdba466962cf47e55e27902e22
Mike Penz <opensource@mikepenz.dev>
2025-12-26 10:17:34 +0100

    introduce new wear focused material 3 compose module

---
+++ aboutlibraries-compose-wear-m3/src/main/kotlin/com/mikepenz/aboutlibraries/ui/compose/wear/preview/M3SnapshotTests.kt
@@ -0,0 +1,40 @@
+package com.mikepenz.aboutlibraries.ui.compose.wear.preview
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.wear.compose.material3.MaterialTheme
+import androidx.wear.compose.ui.tooling.preview.WearPreviewDevices
+import com.mikepenz.aboutlibraries.ui.compose.wear.LibrariesContainer
+
+@Composable
+fun Theme(isDarkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) = MaterialTheme() { content() }
+
+@WearPreviewDevices
+@Composable
+fun PreviewLibraries() = Theme {
+    Box {
+        LibrariesContainer(libraries = fakeData, showDescription = false)
+    }
+}
+
+@WearPreviewDevices
+@Composable
+fun PreviewLibrariesOff() = Theme {
+    Box {
+        LibrariesContainer(libraries = fakeData, showAuthor = false, showLicenseBadges = false, showVersion = false)
+    }
+}
+
+@WearPreviewDevices
+@Composable
+fun PreviewLibraryRTL() = Theme {
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+        Box {
+            LibrariesContainer(fakeData)
+        }
+    }
+}
