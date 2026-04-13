904d859ed09d3dcf5725bdb68c451db92a2a8447
Mike Penz <opensource@mikepenz.dev>
2026-04-13 16:21:01 +0200

    chore: update to 0.14.4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,7 +25,7 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("baseLibs") {
-            from("com.mikepenz:version-catalog:0.14.2")
+            from("com.mikepenz:version-catalog:0.14.4")
         }
     }
 }
